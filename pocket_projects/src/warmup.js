b975e26aae5b9be9536d2f95354fb7e31431e603
Andrew Kihs <ajkihs@gmail.com>
2021-04-23 13:43:41 -0400

    Finish Phase 0

--- pocket_projects/src/warmup.js
+++ pocket_projects/src/warmup.js
@@ -2,7 +2,11 @@
 const partyHeader = document.getElementById('party');
 
 export const htmlGenerator = (string, htmlElement) => {
-
+  // string converted into <p> tag
+  const p = document.createElement("p");
+  p.innerText = string;
+  // append to input html element
+  htmlElement.appendChild(p);
 };
 
 htmlGenerator('Party Time.', partyHeader);
